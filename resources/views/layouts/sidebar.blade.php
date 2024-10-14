2337fadef54bf83ac6369bdc505301b1423781ba
Erik Zerpa <zerpaerik@gmail.com>
2024-10-14 19:46:33 -0400

    aaa

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -320,6 +320,229 @@
     </div>
     @endif
 
+    @if(\Auth::user()->rol == 4)
+
+<a href="{{route('home')}}" class="brand-link">
+<img src="logo.png" class="img-circle elevation-2" alt="User Image" width="40">
+      
+      <span class="brand-text font-weight-light" style="margin-left:10px;">Admin Fertivida</span>
+    </a>
+
+<div class="sidebar">
+      <!-- Sidebar user panel (optional) -->
+    
+
+      <!-- Sidebar Menu -->
+      <nav class="mt-2">
+
+        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+          <!-- Add icons to the links using the .nav-icon class
+               with font-awesome or any other icon font library -->
+
+              
+          <li class="nav-item has-treeview">
+            <a href="#" class="nav-link">
+              <i class="nav-icon fas fa-money-check-alt"></i>
+              <p>
+                Consultas
+                <i class="fas fa-angle-left right"></i>
+              </p>
+            </a>
+            <ul class="nav nav-treeview">
+
+
+            <li class="nav-item">
+                <a href="{{route('consultas.index')}}" class="nav-link">
+                  <i class="fas fa-hand-holding-usd nav-icon"></i>
+                  <p>Lista de Consultas</p>
+                </a>
+              </li>
+
+              
+              
+            </ul>
+          </li>
+
+          <li class="nav-item has-treeview">
+            <a href="#" class="nav-link">
+              <i class="nav-icon fas fa-money-check-alt"></i>
+              <p>
+                Recetas
+                <i class="fas fa-angle-left right"></i>
+              </p>
+            </a>
+            <ul class="nav nav-treeview">
+
+
+            <li class="nav-item">
+                <a href="{{route('recetas.index')}}" class="nav-link">
+                  <i class="fas fa-hand-holding-usd nav-icon"></i>
+                  <p>Lista</p>
+                </a>
+              </li>
+
+              
+              
+            </ul>
+          </li>
+
+          <li class="nav-item has-treeview">
+            <a href="#" class="nav-link">
+              <i class="nav-icon fas fa-money-check-alt"></i>
+              <p>
+                Ordenes
+                <i class="fas fa-angle-left right"></i>
+              </p>
+            </a>
+            <ul class="nav nav-treeview">
+
+
+            <li class="nav-item">
+                <a href="{{route('ordenes.index')}}" class="nav-link">
+                  <i class="fas fa-hand-holding-usd nav-icon"></i>
+                  <p>Lista</p>
+                </a>
+              </li>
+
+              
+              
+            </ul>
+          </li>
+          
+          <li class="nav-item has-treeview">
+            <a href="#" class="nav-link">
+              <i class="nav-icon fas fa-money-check-alt"></i>
+              <p>
+               Resultados
+                <i class="fas fa-angle-left right"></i>
+              </p>
+            </a>
+            <ul class="nav nav-treeview">
+
+
+            <li class="nav-item">
+                <a href="{{route('resultados.index')}}" class="nav-link">
+                  <i class="fas fa-hand-holding-usd nav-icon"></i>
+                  <p>Pendientes Servicio</p>
+                </a>
+              </li>
+
+
+              <li class="nav-item">
+                <a href="{{route('resultados.index1')}}" class="nav-link">
+                  <i class="fas fa-cart-arrow-down nav-icon"></i>
+                  <p>Pendientes Laboratorio</p>
+                </a>
+              </li>
+
+              <li class="nav-item">
+                <a href="{{route('resultados.indexg')}}" class="nav-link">
+                  <i class="fas fa-hand-holding-usd nav-icon"></i>
+                  <p>Guardados Servicio</p>
+                </a>
+              </li>
+
+
+              <li class="nav-item">
+                <a href="{{route('resultados.indexg1')}}" class="nav-link">
+                  <i class="fas fa-cart-arrow-down nav-icon"></i>
+                  <p>Guardados Laboratorio</p>
+                </a>
+              </li>
+
+            
+            
+              
+            </ul>
+          </li>
+
+         
+          <li class="nav-item has-treeview">
+            <a href="#" class="nav-link">
+              <i class="nav-icon fas fa-money-check-alt"></i>
+              <p>
+                Proformas
+                <i class="fas fa-angle-left right"></i>
+              </p>
+            </a>
+            <ul class="nav nav-treeview">
+
+
+            <li class="nav-item">
+                <a href="{{route('proformas.index')}}" class="nav-link">
+                  <i class="fas fa-hand-holding-usd nav-icon"></i>
+                  <p>Lista</p>
+                </a>
+              </li>
+
+              
+              
+            </ul>
+          </li>
+
+
+
+          <li class="nav-item has-treeview">
+            <a href="#" class="nav-link">
+              <i class="nav-icon fas fa-money-check-alt"></i>
+              <p>
+                Reportes
+                <i class="fas fa-angle-left right"></i>
+              </p>
+            </a>
+            <ul class="nav nav-treeview">
+
+
+           
+
+              <li class="nav-item">
+                <a href="{{route('historial.pacientes')}}" class="nav-link">
+                  <i class="fas fa-hand-holding-usd nav-icon"></i>
+                  <p>Historial de Pacientes</p>
+                </a>
+              </li>
+
+
+            
+            </ul>
+          </li>
+
+
+          <li class="nav-item has-treeview">
+            <a href="#" class="nav-link">
+              <i class="nav-icon fas fa-tools"></i>
+              <p>
+                Administrativo
+                <i class="fas fa-angle-left right"></i>
+              </p>
+            </a>
+            <ul class="nav nav-treeview">
+
+            <li class="nav-item">
+                <a href="{{route('users.password')}}" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Modificar Contraseña</p>
+                </a>
+              </li>
+          
+            
+            </ul>
+          </li>
+
+          
+
+
+          
+         
+          
+         
+         
+      </nav>
+      <!-- /.sidebar-menu -->
+    </div>
+    @endif
+
+
   
 
 
