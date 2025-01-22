1bef3d10922b26262755985357eb02f4073d2112
Erik Zerpa <zerpaerik@gmail.com>
2025-01-22 19:17:28 -0400

    aaa

--- resources/views/documentos/create.blade.php
+++ resources/views/documentos/create.blade.php
@@ -61,12 +61,12 @@
       <div class="container-fluid">
         <div class="row mb-2">
           <div class="col-sm-6">
-            <h1 class="m-0 text-dark">Proformas</h1>
+            <h1 class="m-0 text-dark">Documentos</h1>
           </div><!-- /.col -->
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
               <li class="breadcrumb-item"><a href="#">Home</a></li>
-              <li class="breadcrumb-item active">Crear Proformas</li>
+              <li class="breadcrumb-item active">Crear Documentos</li>
             </ol>
           </div><!-- /.col -->
         </div><!-- /.row -->
